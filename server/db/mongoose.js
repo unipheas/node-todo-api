91ef7c05f6a15c530ba661ff1b0c33c242a17e40
Brian Phillips <2313265939@qq.com>
2017-03-28 00:15:58 +0800

    Add DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://polyphasicdevs:8xAa7YakNkNjHqCNQvKL6DXY2NKeT6wsc6Fr@ds135800.mlab.com:35800/node-todo-api');
+// mongoose.connect('mongodb://polyphasicdevs:8xAa7YakNkNjHqCNQvKL6DXY2NKeT6wsc6Fr@ds135800.mlab.com:35800/node-todo-api');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
