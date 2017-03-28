28286592ffae3e8634016a4391706a1259011141
Brian Phillips <2313265939@qq.com>
2017-03-29 01:51:54 +0800

    test db setup

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://polyphasicdevs:8xAa7YakNkNjHqCNQvKL6DXY2NKeT6wsc6Fr@ds135800.mlab.com:35800/node-todo-api');
-// mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
