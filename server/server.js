944ffaa5453518c9b3fc0487e58aad574d12871c
Brian Phillips <2313265939@qq.com>
2017-03-29 01:13:23 +0800

    add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
@@ -40,13 +40,11 @@ app.get('/todos/:id', (req, res) => {
   }
 
   Todo.findById(id).then((todo) => {
-
     if (!todo) {
       return res.status(404).send();
     }
 
     res.send({todo});
-
   }).catch((e) => {
     res.status(400).send();
   });
@@ -65,15 +63,39 @@ app.delete('/todos/:id', (req, res) => {
     }
 
     res.send({todo});
-
   }).catch((e) => {
     res.status(400).send();
   });
+});
 
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
 });
 
 app.listen(port, () => {
-  console.log(`Started up at ${port}`);
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
